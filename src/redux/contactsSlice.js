0ec8a7caa0914f93c9440b6f675d2a56a1149ca4
YuriyBeregovoy <rik0681@gmail.com>
2023-09-29 20:26:12 +0300

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,10 +1,6 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice} from "@reduxjs/toolkit";
+import { fetchContacts } from "./operations";
 
-// const contactsInitialState =
-//   [{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' }];
   
 
 const contactsSlice = createSlice({
@@ -15,31 +11,12 @@ const contactsSlice = createSlice({
     error: null,
   },
 
-  reducers: {
-    addContacts: {
-      reducer(state, action) {
-        return state.concat(action.payload);
-      },
-      prepare({name, number}) {
-        return {
-          payload: {
-            name,
-            number,
-            id: nanoid(),
-          },
-        };
-      }
-    },
-    deleteContacts(state, action) {
-       const index = state.findIndex(contact => contact.id === action.payload);
-      state.splice(index, 1);
-        if (index !== -1) {
-        state.splice(index, 1);
-      }
-    
-    },
-   
-  }
+  extraReducers: {
+    [fetchContacts.pending](state, action) {},
+    [fetchContacts.fulfilled](state, action) {},
+    [fetchContacts.rejected](state, action) {},
+  },
+  
 });
+
 export const contactsReducer = contactsSlice.reducer;
-export const { addContacts, deleteContacts} = contactsSlice.actions;
