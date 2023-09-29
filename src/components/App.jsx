4799ce67a752b58235bea5b3a692fd524e4c0bba
YuriyBeregovoy <rik0681@gmail.com>
2023-09-29 21:51:22 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,27 @@
+import { useSelector } from "react-redux";
 import { ContactForm } from "./ContactForm/ContactForm";
 import { ContactList } from "./ContactList/ContactList";
 import { Filter } from "./Filter/Filter";
 import { Layout } from "./Layout";
+import {  useEffect } from 'react';
+import { fetchContacts } from 'redux/operations';
+import { getIsLoading, getError } from "redux/contactsSlice";
+import { useDispatch } from 'react-redux';
 
 export const App = () => {
+  const dispatch = useDispatch();
+   const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
     return (
       <Layout>
       <h1>Phonebook</h1>
-        <ContactForm/>
-     
+        <ContactForm />
+        {isLoading && !error ? <b>Request in progress...</b> : null}
       <h2>Contacts</h2>
         <Filter />
         <ContactList/>
@@ -17,4 +29,5 @@ export const App = () => {
     );
 
 
-  }
+}
+  
