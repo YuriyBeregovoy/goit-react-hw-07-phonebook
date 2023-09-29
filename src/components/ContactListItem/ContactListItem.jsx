f81ed71755ad5de886b83d42bc7ea1a3806805b4
YuriyBeregovoy <rik0681@gmail.com>
2023-09-29 21:51:28 +0300

    Update ContactListItem.jsx

--- src/components/ContactListItem/ContactListItem.jsx
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -1,8 +1,8 @@
 import { Item, ItemButton } from "./ContactListItem.styled."
 
-export const ContactListItem = ({ contact:{id, name, number}, onContactDelete}) => {
+export const ContactListItem = ({ contact:{id, name, phone}, onContactDelete}) => {
   return <Item key={id}>
-            <p>{name}: {number}</p>
+            <p>{name}: {phone}</p>
             <ItemButton type="button" onClick={() => onContactDelete(id)}>Delete</ItemButton>
           </Item> 
 }
