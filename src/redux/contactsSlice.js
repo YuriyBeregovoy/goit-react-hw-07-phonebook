f23bd16b04c5ca6d0d219774590661053ab42b14
YuriyBeregovoy <rik0681@gmail.com>
2023-09-29 20:44:42 +0300

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,6 +1,14 @@
 import { createSlice} from "@reduxjs/toolkit";
-import { fetchContacts } from "./operations";
+import { fetchContacts, addContacts, deleteContacts } from "./operations";
 
+const handlePending = state => {
+  state.isLoading = true;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
   
 
 const contactsSlice = createSlice({
@@ -12,9 +20,33 @@ const contactsSlice = createSlice({
   },
 
   extraReducers: {
-    [fetchContacts.pending](state, action) {},
-    [fetchContacts.fulfilled](state, action) {},
-    [fetchContacts.rejected](state, action) {},
+    [fetchContacts.pending]: handlePending,
+
+    [fetchContacts.fulfilled](state, action) {
+      state.isLoading = false;
+      state.error = null;
+      state.items = action.payload;
+    },
+    [fetchContacts.rejected]: handleRejected,
+
+    [addContacts.pending]: handlePending,
+    [addContacts.fulfilled](state, action) {
+      state.isLoading = false;
+      state.error = null;
+      state.items.push(action.payload);
+    },
+    [addContacts.rejected]: handleRejected,
+
+    [deleteContacts.pending]: handlePending,
+    [deleteContacts.fulfilled](state, action) {
+      state.isLoading = false;
+      state.error = null;
+      const index = state.items.findIndex(
+        task => task.id === action.payload.id
+      );
+      state.items.splice(index, 1);
+    },
+    [deleteContacts.rejected]: handleRejected,
   },
   
 });
