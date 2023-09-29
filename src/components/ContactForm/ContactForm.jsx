fbc4cc76b2d60efdb707c9febdb9ba82f97a1328
YuriyBeregovoy <rik0681@gmail.com>
2023-09-29 20:47:24 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,6 +1,6 @@
 import { FormButton, FormContacts, LabelContacts } from "./ContactForm.styled"
 import { useDispatch } from "react-redux";
-import { addContacts} from "redux/contactsSlice";
+import { addContacts} from "redux/operations";
 import { nanoid } from "@reduxjs/toolkit";
 import { useSelector } from "react-redux";
 
